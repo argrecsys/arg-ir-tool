abea949c0af733fd42913f726bd51678c2a0d94a
Andres Segura-Tinoco <seguraandres7@gmail.com>
2022-02-25 20:19:56 +0100

    Update reranking function
    The document re-ranking function has been updated. The argument score is now calculated as: the logarithm of the weighted sum of the quality and the quantity of arguments in a document.

--- code/ArgumentIR/src/es/uam/irg/io/IOManager.java
+++ code/ArgumentIR/src/es/uam/irg/io/IOManager.java
@@ -31,7 +31,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
